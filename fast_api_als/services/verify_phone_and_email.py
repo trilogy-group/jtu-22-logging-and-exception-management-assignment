dce4eaef031905b9c24fec25b98205951151db61
Shivam-TI <shivam.gupta@trilogy.com>
2022-05-17 17:26:02 +0000

    Logging created for verify_phone_and_email.py

--- fast_api_als/services/verify_phone_and_email.py
+++ fast_api_als/services/verify_phone_and_email.py
@@ -8,12 +8,24 @@ from fast_api_als.constants import (
     ALS_DATA_TOOL_SERVICE_URL,
     ALS_DATA_TOOL_REQUEST_KEY)
 
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+formatter = logging.Formatter("%(asctime)s : %(levelname)s : %(name)s : %(message)s")
+
+filehandler = logging.FileHandler('routers.log')
+filehandler.setFormatter(formatter)
+
+logger.addHandler(filehandler)
+
 """
 How can you write log to understand what's happening in the code?
 You also trying to undderstand the execution time factor.
 """
 
 async def call_validation_service(url: str, topic: str, value: str, data: dict) -> None:  # 2
+    logger.info("Call Validation Initiated")
+    start = int(time.time() * 1000.0)
     if value == '':
         return
     async with httpx.AsyncClient() as client:  # 3
@@ -21,9 +33,14 @@ async def call_validation_service(url: str, topic: str, value: str, data: dict) 
 
     r = response.json()
     data[topic] = r
+    end = int(time.time() * 1000.0)
+    logger.info(f"Call validation completed: {end - start}ms")
+
     
 
 async def verify_phone_and_email(email: str, phone_number: str) -> bool:
+    logger.info("Initiated verification of phone and email")
+    start = int(time.time() * 1000.0)
     email_validation_url = '{}?Method={}&RequestKey={}&EmailAddress={}&OutputFormat=json'.format(
         ALS_DATA_TOOL_SERVICE_URL,
         ALS_DATA_TOOL_EMAIL_VERIFY_METHOD,
@@ -44,8 +61,16 @@ async def verify_phone_and_email(email: str, phone_number: str) -> bool:
     )
     if "email" in data:
         if data["email"]["DtResponse"]["Result"][0]["StatusCode"] in ("0", "1"):
+            logger.info("Email validated")
             email_valid = True
+        else:
+            logger.info("Email not found")
     if "phone" in data:
         if data["phone"]["DtResponse"]["Result"][0]["IsValid"] == "True":
+            logger.info("Phone validated")
             phone_valid = True
+        else:
+            logger.info("Phone not found")
+    end = int(time.time() * 1000.0)
+    logger.info(f"Email and phone validation completed: {end - start}ms")
     return email_valid | phone_valid
