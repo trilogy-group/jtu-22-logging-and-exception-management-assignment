70cc86badcf741788979a665dacf61e1e034addc
Shivam-TI <shivam.gupta@trilogy.com>
2022-05-17 15:13:02 +0000

    Logging created for three_pl

--- fast_api_als/routers/three_pl.py
+++ fast_api_als/routers/three_pl.py
@@ -2,6 +2,16 @@ import json
 import logging
 from fastapi import Request
 
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+formatter = logging.Formatter("%(levelname)s : %(name)s : %(message)s")
+
+filehandler = logging.FileHandler('routers.log')
+filehandler.setFormatter(formatter)
+
+logger.addHandler(filehandler)
+
 from fastapi import APIRouter, HTTPException, Depends
 from fast_api_als.database.db_helper import db_helper_session
 from fast_api_als.services.authenticate import get_token
@@ -13,14 +23,20 @@ router = APIRouter()
 
 @router.post("/reset_authkey")
 async def reset_authkey(request: Request, token: str = Depends(get_token)):
+
+    logger.info("Initiated request for resetting of authorization key")
+
     body = await request.body()
     body = json.loads(body)
     provider, role = get_user_role(token)
     if role != "ADMIN" and (role != "3PL"):
-        pass
+        raise HTTPException(status_code=401, detail="User Unauthorized")
     if role == "ADMIN":
         provider = body['3pl']
     apikey = db_helper_session.set_auth_key(username=provider)
+
+    logger.info("Request created for resetting of authorization key")
+
     return {
         "status_code": HTTP_200_OK,
         "x-api-key": apikey
@@ -29,15 +45,21 @@ async def reset_authkey(request: Request, token: str = Depends(get_token)):
 
 @router.post("/view_authkey")
 async def view_authkey(request: Request, token: str = Depends(get_token)):
+
+    logger.info("Initiated request for viewing authorization key")
+
     body = await request.body()
     body = json.loads(body)
     provider, role = get_user_role(token)
 
     if role != "ADMIN" and role != "3PL":
-        pass
+        raise HTTPException(status_code=401, detail="User Unauthorized")
     if role == "ADMIN":
         provider = body['3pl']
     apikey = db_helper_session.get_auth_key(username=provider)
+
+    logger.info("Request created for viewing authorization key")
+
     return {
         "status_code": HTTP_200_OK,
         "x-api-key": apikey
