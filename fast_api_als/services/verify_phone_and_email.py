63a52674b693f1c3a0e4ade0ad445009d77e8fd1
rajkotiya1 <man.rajkotiya@trilogy.com>
2022-05-17 15:49:51 +0000

    adding logs in veerify_file

--- fast_api_als/services/verify_phone_and_email.py
+++ fast_api_als/services/verify_phone_and_email.py
@@ -1,12 +1,16 @@
 import time
 import httpx
 import asyncio
-import logging
+from logging import *
 from fast_api_als.constants import (
     ALS_DATA_TOOL_EMAIL_VERIFY_METHOD,
     ALS_DATA_TOOL_PHONE_VERIFY_METHOD,
     ALS_DATA_TOOL_SERVICE_URL,
     ALS_DATA_TOOL_REQUEST_KEY)
+
+
+
+basicConfig(filename='logfile2.log',level = DEBUG , style= '{', format = "{asctime} || {message}")
 
 """
 How can you write log to understand what's happening in the code?
@@ -14,16 +18,19 @@ You also trying to undderstand the execution time factor.
 """
 
 async def call_validation_service(url: str, topic: str, value: str, data: dict) -> None:  # 2
+    start = int(time.time() * 1000.0)
     if value == '':
+        info("during calling validation service valus is empty")
         return
     async with httpx.AsyncClient() as client:  # 3
         response = await client.get(url)
 
     r = response.json()
     data[topic] = r
-    
+    info("time taken : "  + int(time.time() * 1000.0) - start)
 
 async def verify_phone_and_email(email: str, phone_number: str) -> bool:
+    start = int(time.time() * 1000.0)
     email_validation_url = '{}?Method={}&RequestKey={}&EmailAddress={}&OutputFormat=json'.format(
         ALS_DATA_TOOL_SERVICE_URL,
         ALS_DATA_TOOL_EMAIL_VERIFY_METHOD,
@@ -37,15 +44,20 @@ async def verify_phone_and_email(email: str, phone_number: str) -> bool:
     email_valid = False
     phone_valid = False
     data = {}
-
+    info("calling validation services")
     await asyncio.gather(
         call_validation_service(email_validation_url, "email", email, data),
         call_validation_service(phone_validation_url, "phone", phone_number, data),
     )
+    info("time taken by validation services : "  + int(time.time() * 1000.0) - start)
+
     if "email" in data:
         if data["email"]["DtResponse"]["Result"][0]["StatusCode"] in ("0", "1"):
             email_valid = True
+            info("email verification is successfull")
     if "phone" in data:
         if data["phone"]["DtResponse"]["Result"][0]["IsValid"] == "True":
             phone_valid = True
+            info("phone number verification is successfull")
+
     return email_valid | phone_valid
