6c6522f2236e7d6b3f50c559b73089b57a5e3263
CapriciousRebel <code.capriciousrebel@gmail.com>
2022-05-17 15:48:08 +0530

    [Cleanup]: Remove non-existent file imports

--- fast_api_als/main.py
+++ fast_api_als/main.py
@@ -2,19 +2,12 @@ import time
 
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from fast_api_als.routers import users, submit_lead, lead_conversion, reinforcement, oem, three_pl, quicksight
+from routers import submit_lead, lead_conversion, three_pl
 
 app = FastAPI()
-app.include_router(users.router)
 app.include_router(submit_lead.router)
 app.include_router(lead_conversion.router)
-app.include_router(reinforcement.router)
-app.include_router(oem.router)
 app.include_router(three_pl.router)
-app.include_router(quicksight.router)
-
-# only present during test development
-# app.include_router(test_api.router)
 
 origins = ["*"]
 app.add_middleware(
