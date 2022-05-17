58731990d3396648cb48ae9a03f139ebce922b1b
CapriciousRebel <code.capriciousrebel@gmail.com>
2022-05-17 16:01:17 +0530

    [Feature]: Add dummy function for get user role

---
+++ fast_api_als/utils/cognito_client.py
@@ -0,0 +1,15 @@
+def get_user_role(token):
+    '''
+    [Dummy function]
+        Returns the user role based on the provided token
+            Args:
+                token: user's token
+            Returns:
+                provider, role
+    '''
+    if token == '0':
+        return 'ADMIN'
+    elif token == '1':
+        return '3PL'
+    else:
+        raise Exception('Invalid token')
