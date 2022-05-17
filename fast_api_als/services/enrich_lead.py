3febc0fc3c8e1b675ba4af3bfb5f8ec3cf59f968
SoumyadeepTrilogy <soumyadeep.nandi@trilogy.com>
2022-05-17 19:28:53 +0000

    Exception thrown for enrich

--- fast_api_als/services/enrich_lead.py
+++ fast_api_als/services/enrich_lead.py
@@ -9,8 +9,20 @@ from fast_api_als import constants
 """
 what exceptions can be thrown here?
 """
+logger=logging.getlogger(__name__)
+logger.setLevel(logging.DEBUG)
 
+formatter=logging.Formatter('%(asctime)s:%(levelname)s:%(message)s')
+
+file_handler=logging.FileHandler('enrich.log')
+file_handler.setLevel(logging.DEBUG)
+file_handler.setFormatter(formatter)
+
+logger.addHandler(file_handler)
 
 def get_enriched_lead_json(adf_json: dict) -> dict:
-    
-    pass
+    try:
+        pass
+    except KeyError:
+        logger.error("Keyerror exception occured")
+        pass
