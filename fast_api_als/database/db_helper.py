b169ba697762e558b20eaba25caf71aa7c588a5d
SoumyadeepTrilogy <soumyadeep.nandi@trilogy.com>
2022-05-17 19:25:17 +0000

    Common function created and used to log response code with appropriate contextdata

--- fast_api_als/database/db_helper.py
+++ fast_api_als/database/db_helper.py
@@ -9,6 +9,16 @@ from datetime import datetime, timedelta
 
 from fast_api_als import constants
 from fast_api_als.utils.boto3_utils import get_boto3_session
+logger=logging.getlogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+formatter=logging.Formatter('%(asctime)s:%(levelname)s:%(message)s')
+
+file_handler=logging.FileHandler('db.log')
+file_handler.setLevel(logging.DEBUG)
+file_handler.setFormatter(formatter)
+
+logger.addHandler(file_handler)
 """
     the self.table.some_operation(), return a json object and you can find the http code of the executed operation as this :
     res['ResponseMetadata']['HTTPStatusCode']
