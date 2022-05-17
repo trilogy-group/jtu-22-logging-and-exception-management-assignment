8e4ea9245cdf49201f90b004f802782575b99dc6
abhishek-dhanraj-TI <abhishek.dhanraj@trilogy.com>
2022-05-17 19:28:55 +0000

    exception for enrich_lead added

--- fast_api_als/services/enrich_lead.py
+++ fast_api_als/services/enrich_lead.py
@@ -5,6 +5,7 @@ from dateutil import parser
 from fast_api_als.database.db_helper import db_helper_session
 
 from fast_api_als import constants
+logging.basicConfig(format='%(levelname)s %(asctime)s %(message)s')
 
 """
 what exceptions can be thrown here?
@@ -12,4 +13,18 @@ what exceptions can be thrown here?
 
 
 def get_enriched_lead_json(adf_json: dict) -> dict:
-    pass
+    '''
+    1.type error
+    2.key error
+    '''
+    if not isinstance(adf_json, dict):
+        raise TypeError
+
+    #and raise KeyError     for key not present
+    d = dict()
+    try:
+        d['something']= adf_json['something']   #something is not present in adf_json
+    except:
+        raise KeyError()
+    logging.info("Got enriched lead json.")
+    return d
