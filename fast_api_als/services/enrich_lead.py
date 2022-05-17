3612b63c583353294cc968f2abdecd99f6474ccb
Shivam-TI <shivam.gupta@trilogy.com>
2022-05-17 19:26:37 +0000

    Logging created for enrich_lead.py

--- fast_api_als/services/enrich_lead.py
+++ fast_api_als/services/enrich_lead.py
@@ -6,10 +6,24 @@ from fast_api_als.database.db_helper import db_helper_session
 
 from fast_api_als import constants
 
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+formatter = logging.Formatter("%(asctime)s : %(levelname)s : %(name)s : %(message)s")
+
+filehandler = logging.FileHandler('services.log')
+filehandler.setFormatter(formatter)
+
+logger.addHandler(filehandler)
+
+
 """
 what exceptions can be thrown here?
 """
 
 
 def get_enriched_lead_json(adf_json: dict) -> dict:
-    pass
+    try:
+        pass
+    except KeyError:
+        logger.error(f"Key coulnd't be found in the dictionary {adf_json}")
