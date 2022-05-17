91c2990f031917c923ff5b58b7497123e812b6cb
bhoomil-TI <bhoomil.gohel@trilogy.com>
2022-05-17 14:53:29 +0000

    logging and exception handling for routers/lead_conversion.py

--- fast_api_als/routers/lead_conversion.py
+++ fast_api_als/routers/lead_conversion.py
@@ -2,7 +2,7 @@ import json
 from fastapi import APIRouter, Depends
 import logging
 import time
-
+from fastapi import HTTPException
 from fastapi import Request
 from starlette import status
 
@@ -11,11 +11,20 @@ from fast_api_als.quicksight.s3_helper import s3_helper_client
 from fast_api_als.services.authenticate import get_token
 from fast_api_als.utils.cognito_client import get_user_role
 
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+formatter = logging.Formatter("%(levelname)s: %(message)s")
+filehandler = logging.FileHandler('db.log')
+filehandler.setFormatter(formatter)
+logger.addHandler(filehandler)
+
 router = APIRouter()
 
 """
 write proper logging and exception handling
 """
+
+
 
 def get_quicksight_data(lead_uuid, item):
     """
@@ -26,6 +35,7 @@ def get_quicksight_data(lead_uuid, item):
             Returns:
                 S3 data
     """
+    logger.info(f'get_quicksight_data called with lead id {lead_uuid} and item {item}')
     data = {
         "lead_hash": lead_uuid,
         "epoch_timestamp": int(time.time()),
@@ -37,27 +47,35 @@ def get_quicksight_data(lead_uuid, item):
         "3pl": item.get('3pl', 'unknown'),
         "oem_responded": 1
     }
+
     return data, f"{item['make']}/1_{int(time.time())}_{lead_uuid}"
 
 
 @router.post("/conversion")
 async def submit(file: Request, token: str = Depends(get_token)):
+    logger.info('post request recieved at /conversion')
     body = await file.body()
     body = json.loads(str(body, 'utf-8'))
 
     if 'lead_uuid' not in body or 'converted' not in body:
         # throw proper HTTPException
+        # HTTP 400 code exception: BadRequest 
+        raise HTTPException(status_code=400, detail='bad request, body has invalid parameters')       
         pass
-        
+
     lead_uuid = body['lead_uuid']
     converted = body['converted']
-
+    logger.info(f'body has: lead_uuid {lead_uuid} and converted {converted}')
     oem, role = get_user_role(token)
     if role != "OEM":
         # throw proper HTTPException
+        # HTTP 401 code exception: UnauthorizedException
+        raise HTTPException(status_code=401, detail='Unauthorized access, role is not OEM') 
         pass
-
-    is_updated, item = db_helper_session.update_lead_conversion(lead_uuid, oem, converted)
+    
+    logger.info(f'user role: {role}')
+    is_updated, item = db_helper_session.update_lead_conversion(
+        lead_uuid, oem, converted)
     if is_updated:
         data, path = get_quicksight_data(lead_uuid, item)
         s3_helper_client.put_file(data, path)
@@ -67,4 +85,8 @@ async def submit(file: Request, token: str = Depends(get_token)):
         }
     else:
         # throw proper HTTPException
+        # 404
+        raise HTTPException(status_code=404, detail='data not found for lead conversion status update') 
         pass
+
+    logger.info(f'post request accepted for /conversion')
