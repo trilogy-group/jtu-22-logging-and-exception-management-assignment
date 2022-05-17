adaf671460ff0989455bf095f63008485c023f9e
arpit <arpitshukla62@gmail.com>
2022-05-17 21:27:10 +0530

    Update measure_time function to also accept args for the passed lambda

--- fast_api_als/utils/measure_time.py
+++ fast_api_als/utils/measure_time.py
@@ -1,12 +1,12 @@
 import time
 
 
-def measure_time(func):
+def measure_time(func, *args):
     """
     Returns the time taken by the passed lambda function to execute in milliseconds
     along with the result returned by the lambda.
     """
     start_time = time.time()
-    result = func()
+    result = func(*args)
     time_taken = time.time() - start_time
     return int(time_taken * 1000), result
