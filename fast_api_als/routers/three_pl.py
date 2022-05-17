241b9873a47ee1b013af1eb66d96f0eee29794a8
arpit <arpitshukla62@gmail.com>
2022-05-17 19:59:31 +0530

    Add logging and exception handling to three_pl.py router

--- fast_api_als/routers/three_pl.py
+++ fast_api_als/routers/three_pl.py
@@ -6,6 +6,7 @@ from fastapi import APIRouter, HTTPException, Depends
 from fast_api_als.database.db_helper import db_helper_session
 from fast_api_als.services.authenticate import get_token
 from fast_api_als.utils.cognito_client import get_user_role
+from fast_api_als.utils.base_logger import logger
 from starlette.status import HTTP_200_OK, HTTP_401_UNAUTHORIZED
 
 router = APIRouter()
@@ -16,11 +17,16 @@ async def reset_authkey(request: Request, token: str = Depends(get_token)):
     body = await request.body()
     body = json.loads(body)
     provider, role = get_user_role(token)
+    logger.info(f"{provider} - {role} requested for auth key reset")
     if role != "ADMIN" and (role != "3PL"):
-        pass
+        raise HTTPException(
+            status_code=HTTP_401_UNAUTHORIZED,
+            detail=f"Unauthorized user"
+        )
     if role == "ADMIN":
         provider = body['3pl']
     apikey = db_helper_session.set_auth_key(username=provider)
+    logger.info("Auth key reset successful")
     return {
         "status_code": HTTP_200_OK,
         "x-api-key": apikey
@@ -32,9 +38,12 @@ async def view_authkey(request: Request, token: str = Depends(get_token)):
     body = await request.body()
     body = json.loads(body)
     provider, role = get_user_role(token)
-
+    logger.info(f"{provider} - {role} requested to view auth key")
     if role != "ADMIN" and role != "3PL":
-        pass
+        raise HTTPException(
+            status_code=HTTP_401_UNAUTHORIZED,
+            detail=f"Unauthorized user"
+        )
     if role == "ADMIN":
         provider = body['3pl']
     apikey = db_helper_session.get_auth_key(username=provider)
