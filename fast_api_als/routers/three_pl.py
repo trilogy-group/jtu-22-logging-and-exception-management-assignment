e1aedcd69373a303d87c5f8e32649b85cf52c2ad
samyak-ti <samyak.samdariya@trilogy.com>
2022-05-21 15:14:08 +0530

    Logging and exception handling added in three_pl

--- fast_api_als/routers/three_pl.py
+++ fast_api_als/routers/three_pl.py
@@ -10,6 +10,16 @@ from starlette.status import HTTP_200_OK, HTTP_401_UNAUTHORIZED
 
 router = APIRouter()
 
+# Creating and configuring logger
+logging.basicConfig(filename="newFile.log",
+                    format='%(asctime)s %(message)s', filemode='w')
+
+# Creating an object logger
+logger = logging.getLogger()
+
+# Setting the threshold of logger to DEBUG
+logger.setLevel(logging.DEBUG)
+
 
 @router.post("/reset_authkey")
 async def reset_authkey(request: Request, token: str = Depends(get_token)):
@@ -21,6 +31,9 @@ async def reset_authkey(request: Request, token: str = Depends(get_token)):
     if role == "ADMIN":
         provider = body['3pl']
     apikey = db_helper_session.set_auth_key(username=provider)
+
+    logger.info("three_pl:reset_authkey:Auth Key Reset Successfully")
+
     return {
         "status_code": HTTP_200_OK,
         "x-api-key": apikey
@@ -38,6 +51,7 @@ async def view_authkey(request: Request, token: str = Depends(get_token)):
     if role == "ADMIN":
         provider = body['3pl']
     apikey = db_helper_session.get_auth_key(username=provider)
+    logger.info("three_pl:view_authkey:Auth key view Successfully")
     return {
         "status_code": HTTP_200_OK,
         "x-api-key": apikey
