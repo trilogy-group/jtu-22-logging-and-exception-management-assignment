da430600c6cc9657f14e58c9d400d590b631cc18
anubhab-ti <anubhab.das@trilogy.com>
2022-06-01 00:58:59 +0530

    Raised more exceptions in services/enrich_lead.py

--- fast_api_als/services/enrich_lead.py
+++ fast_api_als/services/enrich_lead.py
@@ -12,7 +12,17 @@ what exceptions can be thrown here?
 logger = logging.getLogger(__name__)
 
 def get_enriched_lead_json(adf_json: dict) -> dict:
+    
+    if not isinstance(adf_json, dict):
+        logger.error('Passed adf_json is not a dict')
+        raise TypeError
+    
     if adf_json is None or adf_json == {}:
+        logger.error('adf_json is null or empty')
         raise ValueError
-    else:
-        logger.info('Valid adf_json object passed to get_enriched_lead_json')
+
+    if 'adf' not in adf_json:
+        logger.error('adf_json missing field: adf')
+        raise KeyError
+
+    logger.info('Valid adf_json object passed to get_enriched_lead_json')
