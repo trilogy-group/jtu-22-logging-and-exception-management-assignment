530da6bd98d531ab1e902d99da49b1ab8b26814d
yash-ti <yash.tripathi@trilogy.com>
2022-05-31 15:50:55 +0000

    Added logging and exception handling to enrich_lead.py

--- fast_api_als/services/enrich_lead.py
+++ fast_api_als/services/enrich_lead.py
@@ -6,10 +6,16 @@ from fast_api_als.database.db_helper import db_helper_session
 
 from fast_api_als import constants
 
+logging.basicConfig(format="%(levelname)s: %(asctime)s: %(message)s")
+
 """
 what exceptions can be thrown here?
 """
 
-
 def get_enriched_lead_json(adf_json: dict) -> dict:
-    pass
+    try:
+        make = obj['adf']['prospect']['vehicle']['make']
+        model = obj['adf']['prospect']['vehicle']['model']
+    except Exception as e:
+        logging.error(f"Lead data is incomplete: {e.message}")
+        return adf_json
