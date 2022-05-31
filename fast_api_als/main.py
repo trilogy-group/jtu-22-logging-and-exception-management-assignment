b3f11806c18652b5aa16b403e9ce63ea70ad7605
ashishpathak-ti <106510838+ashishpathak-ti@users.noreply.github.com>
2022-05-31 21:14:29 +0530

    Added logging to main.py

--- fast_api_als/main.py
+++ fast_api_als/main.py
@@ -1,5 +1,5 @@
 import time
-
+import logging
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fast_api_als.routers import users, submit_lead, lead_conversion, reinforcement, oem, three_pl, quicksight
@@ -25,6 +25,8 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+logging.basicConfig(filename='all_logs.log', level=logging.DEBUG, format='%(asctime)s %(filename)s %(levelname)s: %(message)s')
+
 
 @app.get("/")
 def root():
@@ -35,4 +37,5 @@ def root():
 def ping():
     start = time.process_time()
     time_taken = (time.process_time() - start) * 1000
+    logging.info(f"Respone time is - {time_taken} ms")
     return {f"Pong with response time {time_taken} ms"}
