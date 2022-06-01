9b7bb6eaa7d434b54139202dfbd8b7918d854072
yash-ti <yash.tripathi@trilogy.com>
2022-06-01 06:11:47 +0000

    Raised exceptions and validated adf_json in enrich_lead.py

--- fast_api_als/services/enrich_lead.py
+++ fast_api_als/services/enrich_lead.py
@@ -4,15 +4,21 @@ from dateutil import parser
 from fast_api_als.database.db_helper import db_helper_session
 from fast_api_als.utils.logger import logger
 from fast_api_als import constants
+from fast_api_als.utils import validate_adf_values
 
 """
 what exceptions can be thrown here?
 """
 
 def get_enriched_lead_json(adf_json: dict) -> dict:
-    try:
-        make = obj['adf']['prospect']['vehicle']['make']
-        model = obj['adf']['prospect']['vehicle']['model']
-    except Exception as e:
-        logger.error(f"Lead data is incomplete: {e.message}")
-        return adf_json
+    
+    if not adf_json.has_key('adf'):
+        logger.error("adf missing in lead json sent for enrichment")
+        raise KeyError("adf is missing from adf_json passed for enrichment")
+    
+    res = validate_adf_values(adf_json)
+    if res['status'] == 'Rejected':
+        logger.error(f"adf passed for enriching is invalid: {res['message']}")
+        raise ValueError("adf_json passed for enrichment is missing some values")
+    
+    logger.info("adf_json passed for enrichment is valid")
