5125a22bebcd200e178c4327cb8e13d4a0b714e1
mdtanzeel-ti <md.tanzeel@trilogy.com>
2022-06-14 14:26:41 +0000

    Exception handling and logging statements added

--- fast_api_als/services/enrich_lead.py
+++ fast_api_als/services/enrich_lead.py
@@ -10,6 +10,16 @@ from fast_api_als import constants
 what exceptions can be thrown here?
 """
 
+logging.basicConfig(level=logging.INFO,format='%(levelname)s:%(asctime)s:%(module)s')
 
 def get_enriched_lead_json(adf_json: dict) -> dict:
-    pass
+    # Checking data-type
+    valid=isinstance(dict, adf_json)
+
+    if not valid:
+        logging.error("Invalid datatype dictionary file")
+        raise TypeError()
+
+    logging.info("Enriched lead json.")
+    dic=dict()
+    return dic
