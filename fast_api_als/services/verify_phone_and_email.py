9cf5bd284d39abdfa4a46b61d87eb1256ee4e1da
msatanzeel-ti <msa.tanzeel@trilogy.com>
2022-06-14 15:00:45 +0000

    Added the logging statements and the time factor information through logging in verify_phone_and_email

--- fast_api_als/services/verify_phone_and_email.py
+++ fast_api_als/services/verify_phone_and_email.py
@@ -2,6 +2,8 @@ import time
 import httpx
 import asyncio
 import logging
+logging.basicConfig(level=logging.DEBUG , format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
 from fast_api_als.constants import (
     ALS_DATA_TOOL_EMAIL_VERIFY_METHOD,
     ALS_DATA_TOOL_PHONE_VERIFY_METHOD,
@@ -10,20 +12,26 @@ from fast_api_als.constants import (
 
 """
 How can you write log to understand what's happening in the code?
-You also trying to undderstand the execution time factor.
+You also trying to understand the execution time factor.
 """
 
 async def call_validation_service(url: str, topic: str, value: str, data: dict) -> None:  # 2
+    start_time = time.process()
     if value == '':
+        logging.debug(topic + " doesn't contain any value in the call_validation_service call" )
         return
     async with httpx.AsyncClient() as client:  # 3
         response = await client.get(url)
+    time_taken = (time.process() - start_time ) * 1000
+
+    logging.info("The time taken to resolve the Validation request through API is : " + str(time_taken))
 
     r = response.json()
     data[topic] = r
     
 
 async def verify_phone_and_email(email: str, phone_number: str) -> bool:
+    start_time = time.process()
     email_validation_url = '{}?Method={}&RequestKey={}&EmailAddress={}&OutputFormat=json'.format(
         ALS_DATA_TOOL_SERVICE_URL,
         ALS_DATA_TOOL_EMAIL_VERIFY_METHOD,
@@ -48,4 +56,10 @@ async def verify_phone_and_email(email: str, phone_number: str) -> bool:
     if "phone" in data:
         if data["phone"]["DtResponse"]["Result"][0]["IsValid"] == "True":
             phone_valid = True
+    time_taken = (time.process() - start_time ) * 1000
+    if not (email_valid or phone_valid):
+        logging.info("The phone number and email are not valid and time taken is " + str(time_taken))
+    else :
+        logging.info("The validation of phone number or email was successful and time taken is " + str(time_taken))
+    
     return email_valid | phone_valid
