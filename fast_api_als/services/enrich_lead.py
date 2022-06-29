1b49c24dab1ba2b0a19e28e8e7b81d8a5261cbc6
arpan-ti <arpan.gupta@trilogy.com>
2022-06-29 20:35:36 +0000

    Updated file with exception and logs

--- fast_api_als/services/enrich_lead.py
+++ fast_api_als/services/enrich_lead.py
@@ -9,7 +9,24 @@ from fast_api_als import constants
 """
 what exceptions can be thrown here?
 """
+# Creating logger file and configuring
+logging.basicConfig(filename="newFile.log", format='%(asctime)s %(message)s', filemode='w')
+
+# Creating object logger
+logger = logging.getLogger()
+
+# Setting threshold of logger to DEBUG
+logger.setLevel(logging.DEBUG)
 
 
 def get_enriched_lead_json(adf_json: dict) -> dict:
     pass
+
+    # Checking if the adf_json dictionary is empty
+    bool_empty= not bool(adf_json)
+
+    if(boolempty):
+        logger.info("enrich_lead: get_enriched_lead_json: adf_json is empty")
+        return Exception("Dictionary is empty")
+    logger.info("enrich_lead: get_enriched_lead: Function ran successfully")
+    return adj_json
