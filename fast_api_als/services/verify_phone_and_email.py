b2f0887f4366d8686d7caf77ed1346dd5ca7f1fd
arpan-ti <arpan.gupta@trilogy.com>
2022-06-29 20:48:14 +0000

    Updated with Logging

--- fast_api_als/services/verify_phone_and_email.py
+++ fast_api_als/services/verify_phone_and_email.py
@@ -12,8 +12,18 @@ from fast_api_als.constants import (
 How can you write log to understand what's happening in the code?
 You also trying to undderstand the execution time factor.
 """
+# Creating logger file and configuring
+logging.basicConfig(filename="newFile.log", format='%(asctime)s %(message)s', filemode='w')
+
+# Creating object logger
+logger = logging.getLogger()
+
+# Setting threshold of logger to DEBUG
+logger.setLevel(logging.DEBUG)
 
 async def call_validation_service(url: str, topic: str, value: str, data: dict) -> None:  # 2
+    start_time = int(time.process_time()*1000.0)
+
     if value == '':
         return
     async with httpx.AsyncClient() as client:  # 3
@@ -21,19 +31,27 @@ async def call_validation_service(url: str, topic: str, value: str, data: dict) 
 
     r = response.json()
     data[topic] = r
+    end_time = int(time.process_time()*1000.0)
+    logger.info(f"verify_phone_and_email: call_validation_service: Took {end_time-start_time}ms to execute the function")
+    logger.info("verify_phone_and_email: call_validation_service: Function executed successfully")
     
 
 async def verify_phone_and_email(email: str, phone_number: str) -> bool:
+    #Start time
+    start_time = int(time.process_time()*1000.0)
+
     email_validation_url = '{}?Method={}&RequestKey={}&EmailAddress={}&OutputFormat=json'.format(
         ALS_DATA_TOOL_SERVICE_URL,
         ALS_DATA_TOOL_EMAIL_VERIFY_METHOD,
         ALS_DATA_TOOL_REQUEST_KEY,
         email)
+    logger.info("verify_phone_and_email: verify_phone_and_email: Generated email validation url")
     phone_validation_url = '{}?Method={}&RequestKey={}&PhoneNumber={}&OutputFormat=json'.format(
         ALS_DATA_TOOL_SERVICE_URL,
         ALS_DATA_TOOL_PHONE_VERIFY_METHOD,
         ALS_DATA_TOOL_REQUEST_KEY,
         phone_number)
+    logger.info("verify_phone_and_email: verify_phone_and_email: Generated phone validation url")
     email_valid = False
     phone_valid = False
     data = {}
@@ -48,4 +66,6 @@ async def verify_phone_and_email(email: str, phone_number: str) -> bool:
     if "phone" in data:
         if data["phone"]["DtResponse"]["Result"][0]["IsValid"] == "True":
             phone_valid = True
+    end_time = int(time.process_time()*1000.0)
+    logger.info(f"verify_phone_and_email: verify_phone_and_email: Function executed in {end_time-start_time}ms to run")
     return email_valid | phone_valid
