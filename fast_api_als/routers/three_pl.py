5a9d579db6856d2d6cbf65bee54e2d2031f5b9a6
prantaneel-ti <prantaneel.debnath@trilogy.com>
2022-12-13 11:25:46 +0000

    added logging and exception handling to routers

--- fast_api_als/routers/three_pl.py
+++ fast_api_als/routers/three_pl.py
@@ -1,6 +1,6 @@
 import json
 import logging
-from fastapi import Request
+from fastapi import Request, HTTPException
 
 from fastapi import APIRouter, HTTPException, Depends
 from fast_api_als.database.db_helper import db_helper_session
@@ -10,17 +10,26 @@ from starlette.status import HTTP_200_OK, HTTP_401_UNAUTHORIZED
 
 router = APIRouter()
 
+log = logging.getLogger(__name__)
 
 @router.post("/reset_authkey")
 async def reset_authkey(request: Request, token: str = Depends(get_token)):
-    body = await request.body()
-    body = json.loads(body)
+    try:
+        body = await request.body()
+        body = json.loads(body)
+        log.info("Body parsed successfully.")
+    except json.JSONDecodeError as jde : 
+        log.error(f'Request body not a valid JSON document: {jde}')
+    except Exception as e:
+        log.error(f'Request body parse failed: {e}')
     provider, role = get_user_role(token)
     if role != "ADMIN" and (role != "3PL"):
+        raise HTTPException(status_code=401, detail='User with role other than ADMIN or 3PL are not authorized.')
         pass
     if role == "ADMIN":
         provider = body['3pl']
     apikey = db_helper_session.set_auth_key(username=provider)
+    log.info(f'Successfully set the API auth key for the user :{provider} having role: {role}')
     return {
         "status_code": HTTP_200_OK,
         "x-api-key": apikey
@@ -29,15 +38,23 @@ async def reset_authkey(request: Request, token: str = Depends(get_token)):
 
 @router.post("/view_authkey")
 async def view_authkey(request: Request, token: str = Depends(get_token)):
-    body = await request.body()
-    body = json.loads(body)
+    try:
+        body = await request.body()
+        body = json.loads(body)
+        log.info('Request body parsed successfully.')
+    except json.JSONDecodeError as jde:
+        log.error(f'Request body is not a valid JSON document: {jde}')
+    except Exception as e:
+        log.error(f'Parsing of request body failed: {e}')
     provider, role = get_user_role(token)
 
     if role != "ADMIN" and role != "3PL":
+        raise HTTPException(status_code=401, detail='User with role other than ADMIN or 3PL are not authorized.')
         pass
     if role == "ADMIN":
         provider = body['3pl']
     apikey = db_helper_session.get_auth_key(username=provider)
+    log.info(f'Successfully set the API auth key for the user :{provider} having role: {role}')
     return {
         "status_code": HTTP_200_OK,
         "x-api-key": apikey
