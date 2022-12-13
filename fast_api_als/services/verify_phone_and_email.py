d51a3aec7f5d44d8c1e3b629e64ecb4106e0e7a7
prantaneel-ti <prantaneel.debnath@trilogy.com>
2022-12-13 11:29:14 +0000

    added logging and exception handling to services

--- fast_api_als/services/verify_phone_and_email.py
+++ fast_api_als/services/verify_phone_and_email.py
@@ -13,11 +13,20 @@ How can you write log to understand what's happening in the code?
 You also trying to undderstand the execution time factor.
 """
 
+log = logging.getLogger(__name__)
+
 async def call_validation_service(url: str, topic: str, value: str, data: dict) -> None:  # 2
     if value == '':
         return
     async with httpx.AsyncClient() as client:  # 3
-        response = await client.get(url)
+        try:
+            response = await client.get(url)
+            log.info(f'Request successful to client URL {url}')
+        except httpx.RequestError as re:
+            log.error(f'Error occured while requesting {re.request.url}')
+        except Exception as e:
+            log.error(f'Error occured while requesting URL due to {e}')
+            
 
     r = response.json()
     data[topic] = r
@@ -38,14 +47,20 @@ async def verify_phone_and_email(email: str, phone_number: str) -> bool:
     phone_valid = False
     data = {}
 
+    start = int(time.time()*1000)
     await asyncio.gather(
         call_validation_service(email_validation_url, "email", email, data),
         call_validation_service(phone_validation_url, "phone", phone_number, data),
     )
+    end = int(time.time()*1000)
+    time_taken = end - start
+    log.info(f'[{time_taken}] Validation done successfully.')
     if "email" in data:
         if data["email"]["DtResponse"]["Result"][0]["StatusCode"] in ("0", "1"):
             email_valid = True
+            log.info(f'User verified by email: {email}')
     if "phone" in data:
         if data["phone"]["DtResponse"]["Result"][0]["IsValid"] == "True":
             phone_valid = True
+            log.info(f'User verified by phone number: {phone_number}')
     return email_valid | phone_valid
