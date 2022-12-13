6087b042793d7b1f59b2e021bc41ec4c33f5b371
aniket-ti <aniket.agrawal@trilogy.com>
2022-12-13 20:28:47 +0000

    Logging in verify_phone_and_email.py

--- fast_api_als/services/verify_phone_and_email.py
+++ fast_api_als/services/verify_phone_and_email.py
@@ -14,7 +14,9 @@ You also trying to undderstand the execution time factor.
 """
 
 async def call_validation_service(url: str, topic: str, value: str, data: dict) -> None:  # 2
+    start_time = int(time.process_time()*1000.0)
     if value == '':
+        logging.info("%s missing", topic)
         return
     async with httpx.AsyncClient() as client:  # 3
         response = await client.get(url)
@@ -22,8 +24,11 @@ async def call_validation_service(url: str, topic: str, value: str, data: dict) 
     r = response.json()
     data[topic] = r
     
+    end_time = int(time.process_time()*1000.0)
+    logger.info(f"verify_phone_and_email: call_validation_service: Took {end_time-start_time}ms to execute the function")
 
 async def verify_phone_and_email(email: str, phone_number: str) -> bool:
+    start_time = int(time.process_time()*1000.0)
     email_validation_url = '{}?Method={}&RequestKey={}&EmailAddress={}&OutputFormat=json'.format(
         ALS_DATA_TOOL_SERVICE_URL,
         ALS_DATA_TOOL_EMAIL_VERIFY_METHOD,
@@ -38,14 +43,24 @@ async def verify_phone_and_email(email: str, phone_number: str) -> bool:
     phone_valid = False
     data = {}
 
+    logging.info("Calling validation service for email and phone")
     await asyncio.gather(
         call_validation_service(email_validation_url, "email", email, data),
         call_validation_service(phone_validation_url, "phone", phone_number, data),
     )
+    logging.info("Call to validation service completed for email and phone")
     if "email" in data:
         if data["email"]["DtResponse"]["Result"][0]["StatusCode"] in ("0", "1"):
             email_valid = True
+            logging.info("Email valid")
+        else: logging.info("Email invalid")
+    else: logging.info("Email not validated")
     if "phone" in data:
         if data["phone"]["DtResponse"]["Result"][0]["IsValid"] == "True":
             phone_valid = True
+            logging.info("Phone valid")
+        else: logging.info("Phone invalid")
+    else: logging.info("Phone not validated")
+    end_time = int(time.process_time()*1000.0)
+    logger.info(f"verify_phone_and_email: verify_phone_and_email: Function executed in {end_time-start_time}ms to run")
     return email_valid | phone_valid
