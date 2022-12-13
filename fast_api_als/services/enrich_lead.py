2b337d408d1bc4c060528d3ef00327f5f0b20a6d
mohit-ti <mohit.agarwal@trilogy.com>
2022-12-13 18:56:55 +0000

    Added Exception in enrich_lead.py

--- fast_api_als/services/enrich_lead.py
+++ fast_api_als/services/enrich_lead.py
@@ -3,13 +3,9 @@ import time
 import logging
 from dateutil import parser
 from fast_api_als.database.db_helper import db_helper_session
-
 from fast_api_als import constants
 
-"""
-what exceptions can be thrown here?
-"""
-
-
 def get_enriched_lead_json(adf_json: dict) -> dict:
-    pass
+    if not isinstance(adf_json, list):
+        raise TypeError(f"Expected {dict}, got {type(x)}")
+    return adf_json
