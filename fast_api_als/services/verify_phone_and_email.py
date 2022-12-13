89e9509f7aac0ec8f07e56c7aaa4459dbd062fdb
dhananjay-ti <dhananjay.kejriwal@trilogy.com>
2022-12-13 11:04:27 +0000

    Logging added in verify_phone_and_email

--- fast_api_als/services/verify_phone_and_email.py
+++ fast_api_als/services/verify_phone_and_email.py
@@ -15,6 +15,7 @@ You also trying to undderstand the execution time factor.
 
 async def call_validation_service(url: str, topic: str, value: str, data: dict) -> None:  # 2
     if value == '':
+        logging.info("%s missing", topic)
         return
     async with httpx.AsyncClient() as client:  # 3
         response = await client.get(url)
@@ -38,14 +39,23 @@ async def verify_phone_and_email(email: str, phone_number: str) -> bool:
     phone_valid = False
     data = {}
 
+    logging.basicConfig(format='%(asctime)s %(levelname)s %(message)s')
+    logging.info("Calling validation service for email and phone")
     await asyncio.gather(
         call_validation_service(email_validation_url, "email", email, data),
         call_validation_service(phone_validation_url, "phone", phone_number, data),
     )
+    logging.info("Call to validation service completed for email and phone")
     if "email" in data:
         if data["email"]["DtResponse"]["Result"][0]["StatusCode"] in ("0", "1"):
             email_valid = True
+            logging.info("Email valid")
+        else: logging.info("Email invalid")
+    else: logging.info("Email invalid")
     if "phone" in data:
         if data["phone"]["DtResponse"]["Result"][0]["IsValid"] == "True":
             phone_valid = True
+            logging.info("Phone valid")
+        else: logging.info("Phone invalid")
+    else: logging.info("Phone invalid")
     return email_valid | phone_valid
