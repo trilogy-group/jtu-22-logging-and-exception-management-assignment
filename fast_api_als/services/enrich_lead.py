6dc715e635d832240b15d56f25297be4c2d9046c
ritam-ti <ritam.chakraborty@trilogy.com>
2022-12-14 12:02:41 +0530

    Added KeyError and ValueError to enrich_lead.py

--- fast_api_als/services/enrich_lead.py
+++ fast_api_als/services/enrich_lead.py
@@ -1,5 +1,6 @@
 import calendar
 import time
+import json
 import logging
 from dateutil import parser
 from fast_api_als.database.db_helper import db_helper_session
@@ -12,6 +13,10 @@ what exceptions can be thrown here?
 
 
 def get_enriched_lead_json(adf_json: dict) -> dict:
-    if adf_json == {}:
-        logging.warning("Empty JSON")
+    if 'adf' not in adf_json.keys():
+        raise KeyError
+    try:
+        json.loads(adf_json)
+    except ValueError:
+        raise ValueError
     pass
