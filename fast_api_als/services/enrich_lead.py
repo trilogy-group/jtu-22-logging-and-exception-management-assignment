cd47709b1513a483072b31f52e12f25591d7d3c8
dhananjay-ti <dhananjay.kejriwal@trilogy.com>
2022-12-16 04:19:04 +0000

    Key error handling in enrich_lead

--- fast_api_als/services/enrich_lead.py
+++ fast_api_als/services/enrich_lead.py
@@ -14,6 +14,9 @@ what exceptions can be thrown here?
 def get_enriched_lead_json(adf_json: dict) -> dict:
     try:
         json_object = json.dumps(dictionary)
-    except:
+    except ValueError:
         logging.error("Invalid JSON")
         raise ValueError
+    except KeyError:
+        logging.error("Key missing")
+        raise KeyError
